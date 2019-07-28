d6bdacb4cf1c66ce249919736139e8bf8cc773e2
blee <brialee@pdx.edu>
2019-07-28 01:12:55 +0000

    Adds product, buyer, and seller apps

---
+++ buyer_portal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BuyerPortalConfig(AppConfig):
+    name = 'buyer_portal'
---
+++ seller_portal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SellerPortalConfig(AppConfig):
+    name = 'seller_portal'
