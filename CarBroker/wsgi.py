50b512a173438f51d44818a4f4800a94a302328a
blee <brialee@pdx.edu>
2019-07-28 01:06:10 +0000

    initial commit

---
+++ CarBroker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CarBroker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarBroker.settings')
+
+application = get_wsgi_application()
