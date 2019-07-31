de575cbbe82a9eba6103bdb3f1bab2e150ba1781
blee <brialee@pdx.edu>
2019-07-31 09:35:59 -0700

    UI tweaks

--- product_page/urls.py
+++ product_page/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name="index"),
-    path('', views.greyscale, name="greyscale"),
-    path('about', views.about, name="about"),
-    path('register', views.register, name="register"),
-    path('tryit', views.tryit, name="tryit"),
-    path('contact', views.contact, name="contact"),
+    path('', views.index, name="index"),
+    #path('', views.greyscale, name="greyscale"),
+    # path('about', views.about, name="about"),
+    # path('register', views.register, name="register"),
+    # path('tryit', views.tryit, name="tryit"),
+    # path('contact', views.contact, name="contact"),
     path('login', views.login, name="login"),
 ]
--- product_page/views.py
+++ product_page/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {})
-
-def about(request):
-    return render(request, 'about.html', {})
-
-def register(request):
-    return render(request, 'register.html', {})
-
-def contact(request):
-    return render(request, 'contact.html', {})
-
-def tryit(request):
-    return render(request, 'tryit.html', {})
-
-def greyscale(request):
     return render(request, 'greyscale.html', {})
+
+# def about(request):
+#     return render(request, 'about.html', {})
+
+# def register(request):
+#     return render(request, 'register.html', {})
+
+# def contact(request):
+#     return render(request, 'contact.html', {})
+
+# def tryit(request):
+#     return render(request, 'tryit.html', {})
+
+# def greyscale(request):
+#     return render(request, 'greyscale.html', {})
 
 def login(request):
     return render(request, 'login_choice.html', {})
