0a931d525f6e9decb45a1ca79c46c3159aec1733
blee <brialee@pdx.edu>
2019-08-05 14:35:25 -0700

    Removes unnecessary views and paths

--- product_page/urls.py
+++ product_page/urls.py
@@ -4,10 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    #path('', views.greyscale, name="greyscale"),
-    # path('about', views.about, name="about"),
-    # path('register', views.register, name="register"),
-    # path('tryit', views.tryit, name="tryit"),
-    # path('contact', views.contact, name="contact"),
     path('login', views.login, name="login"),
 ]
--- product_page/views.py
+++ product_page/views.py
@@ -3,20 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'greyscale.html', {})
 
-# def about(request):
-#     return render(request, 'about.html', {})
-
-# def register(request):
-#     return render(request, 'register.html', {})
-
-# def contact(request):
-#     return render(request, 'contact.html', {})
-
-# def tryit(request):
-#     return render(request, 'tryit.html', {})
-
-# def greyscale(request):
-#     return render(request, 'greyscale.html', {})
-
 def login(request):
     return render(request, 'login_choice.html', {})
