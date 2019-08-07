e24f6cd81857f886ea0c1b9a43e374cf108baa5d
blee <brialee@pdx.edu>
2019-08-07 15:50:46 -0700

    Remove unnecessary files

--- product_page/views.py
+++ product_page/views.py
@@ -3,16 +3,3 @@ from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'greyscale.html', {})
-
-def login_choice(request):
-    return render(request, 'login_choice.html', {})
-
-def login_auth(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return render(request, 'madeit.html', {})
-    else:
-        return render(request, 'failed.html', {})
