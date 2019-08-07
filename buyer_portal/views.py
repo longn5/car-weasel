aaa203a67a324e1c1b213f8e31f0850740740d24
blee <brialee@pdx.edu>
2019-08-07 14:33:38 -0700

    Bug fix

--- buyer_portal/views.py
+++ buyer_portal/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def buyer_login(request):
     return render(request, 'buyer_login.html', {})
+
+def login(request):
+    return render(request, 'templates/register/login.html', {})
