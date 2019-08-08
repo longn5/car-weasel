c657e4459911320ccff4c1a42362dfac959306b4
blee <brialee@pdx.edu>
2019-08-07 20:47:19 -0700

    Styling fixes for login page

--- static/base/js/gs_custom.js
+++ static/base/js/gs_custom.js
@@ -14,4 +14,17 @@ $(document).ready(function(){
     $('#signup_button').on('click', function(){
         location.href = "/sign-up";
     });
+
+    $('#what_link').on('click', function(){
+        location.href = "/#what";
+    });
+
+    $('#how_link').on('click', function(){
+        location.href = "/#how";
+    });
+
+    $('#contact_link').on('click', function(){
+        location.href = "/#contact";
+    });
+    
 });
