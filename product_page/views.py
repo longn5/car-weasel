fd4b945b70f0aa97b30a8a072bf676f10509bc94
blee <brialee@pdx.edu>
2019-08-25 11:32:13 -0700

    Removes unnecessary data from datasets, adds buyer posts table to buyer dashboard. Updates buyer dash views accordingly.

--- product_page/views.py
+++ product_page/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User, Group
 from django.views.generic import TemplateView
 from dataAPI import api as API
 from django.http import HttpResponse, JsonResponse
+import json
 
 def index(request):
     return render(request, 'greyscale.html', {})
@@ -24,12 +25,32 @@ def welcome(request):
 def goodbye(request):
     return render(request, TemplateView.as_view(template_name="templates/registration/logout.html"))
 
-
 def buyerAddPost(request):
-    if request.user.is_authenticated:
-        usrgrp = Group.objects.get(user=request.user).name
-        if usrgrp == 'buyer':
-            return render(request, 'buyer_dash_addPost.html', {})
+    if request.method == "POST":
+        print("POST with authenticated user")
+        if request.user.is_authenticated:
+            userid = request.user.id
+
+            v = list(request.POST.items())
+            print("Items in POST:")
+            print(v)
+            v.sort(key=lambda tup: tup[0])
+            for x in v:
+                print(x)
+            
+            addstr = request.read().decode('utf-8')
+            print("Request read")
+            print(addstr)
+            #vehicles = json.loads(addstr)
+            #vstr = json.dumps(vstr)
+            #print(vstr)
+        else:
+            return JsonResponse({'response': "Must Be Authenticated to POST"})
+    elif request.method == "GET":
+        if request.user.is_authenticated:
+            usrgrp = Group.objects.get(user=request.user).name
+            if usrgrp == 'buyer':
+                return render(request, 'buyer_dash_addPost.html', {})
 
     return render(request, 'welcome.html', {})
 
